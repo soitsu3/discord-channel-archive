bc644fc3a09fb8155b99cd867b2da31dd73db24c
soitsu <97986952+soitsu3@users.noreply.github.com>
2022-02-08 21:15:46 +0000

    Add new channels

--- rename-script.js
+++ rename-script.js
@@ -1,6 +1,6 @@
 var fs = require("fs")
 
-const dir = fs.opendirSync(".")
+const dir = fs.opendirSync("format")
 let dirent
 while ((dirent = dir.readSync()) !== null) {
     const { name } = dirent
@@ -8,7 +8,7 @@ while ((dirent = dir.readSync()) !== null) {
         let out = name.slice(24)
         out = out.split(" ")[0]
         console.log(out)
-        fs.rename(`${name}`, `${out}.html`, function (err) {
+        fs.rename(`format/${name}`, `html/${out}.html`, function (err) {
             if (err) console.log("ERROR: " + err)
         })
     }
